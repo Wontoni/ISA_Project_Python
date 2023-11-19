78a1e980160c5a80d5d0774654eb31adfcfc529f
Winston <winston_nguyenn@hotmail.com>
2023-11-19 15:55:59 -0800

    Added env variables

--- App.py
+++ App.py
@@ -5,6 +5,8 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+app.config.from_pyfile('settings.py')
+
 @app.route('/')
 def index():
     return "ISA Project Flask Server"
@@ -14,8 +16,8 @@ def index():
 def translate():
     article_en = request.form['original_text']
     translate_code = request.form['translate_code']
-    tokenizer = AutoTokenizer.from_pretrained("SnypzZz/Llama2-13b-Language-translate")
-    model = AutoModelForSeq2SeqLM.from_pretrained("SnypzZz/Llama2-13b-Language-translate")
+    tokenizer = AutoTokenizer.from_pretrained("SnypzZz/Llama2-13b-Language-translate", token=app.config["HUGGINGFACE_TOKEN"])
+    model = AutoModelForSeq2SeqLM.from_pretrained("SnypzZz/Llama2-13b-Language-translate", token=app.config["HUGGINGFACE_TOKEN"])
 
     model_inputs = tokenizer(article_en, return_tensors="pt")
 
@@ -37,6 +39,3 @@ def translate():
     # Chinese (zh_CN),
     # Mongolian (mn_MN),
     # Urdu (ur_PK)
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
